7a5a3cf9c24b9f58d37b412943470de5b80bdf87
ama-prameswari <rachmayanti986@gmail.com>
2024-09-27 15:37:00 +0700

    First commit

---
+++ src/main/java/com/juaracoding/FirstJava.java
@@ -0,0 +1,16 @@
+package com.juaracoding;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello JuaraCoding!");
+        System.out.println("Indonesia");
+
+        if(true) {
+            System.out.println("True");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
